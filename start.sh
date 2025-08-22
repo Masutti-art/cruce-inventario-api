90aa309a4700da7358019e4578ca1e7bbfaf9ba6
Masutti-art <germanhammer@hotmail.com>
2025-08-22 18:23:10 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
-uvicorn main:app --host 0.0.0.0 --port 10000
+#!/usr/bin/env bash
+set -e
+
+# Usa el puerto que Render provee, y si corrés local sin PORT, cae en 10000.
+PORT=${PORT:-10000}
+
+exec uvicorn main:app --host 0.0.0.0 --port "$PORT"
+
